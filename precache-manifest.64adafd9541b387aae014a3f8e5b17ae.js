a8709b79a62a701dde2f65107f7df60ee9ce24da
Shaun <slewin419@gmail.com>
2019-02-12 00:06:27 -0500

    Updates

---
+++ precache-manifest.64adafd9541b387aae014a3f8e5b17ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "eda96b797fc8754d80a7",
+    "url": "./static/js/main.eda96b79.chunk.js"
+  },
+  {
+    "revision": "0709456cec51b6d65fe9",
+    "url": "./static/js/1.0709456c.chunk.js"
+  },
+  {
+    "revision": "eda96b797fc8754d80a7",
+    "url": "./static/css/main.78c33174.chunk.css"
+  },
+  {
+    "revision": "86054eb2925ca097fcd7cfc34c7b93f6",
+    "url": "./index.html"
+  }
+];
